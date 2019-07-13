cf59b92386edd57db92a41ecde2b96cf2a88956c
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-07-13 18:13:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 function preventRefreshOnSubmit(){
     document.querySelector('form').addEventListener('submit', function(event){
       event.preventDefault()
+
     })
 }
 
 preventRefreshOnSubmit()
+
+let input = document.querySelector('input')
+
+function retrieveEmployeeInformation(){
+  return input.value
+}
